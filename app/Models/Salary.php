82dcc4c1c5affd47d94b9f01098a8f05bd71f865
ymbyuki <ymbyuki0104@gmail.com>
2022-12-29 23:25:31 +0900

    不要なデバッグのネームスペースが入っていたため削除

--- app/Models/Salary.php
+++ app/Models/Salary.php
@@ -6,8 +6,6 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-use function Psy\debug;
-
 class Salary extends Model
 {
     use HasFactory;
